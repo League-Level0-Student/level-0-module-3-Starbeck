47ef62974862496483b498589b6db9e8fe0730fc
League Student <league@iMac.attlocal.net>
2019-05-25 17:28:02 -0700

    Continued Level 0 Module 3
    Improved Eclipse project, ObedientRobot

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -5,20 +5,47 @@ import org.jointheleague.graphical.robot.Robot;
 public class ObedientRobot {
 	static Robot c = new Robot();
 	public static void main(String[] args) {
-		c.setWindowColor(125,125,125);
+		c.setWindowColor(205,205,205);
+		c.setX(50);
+		c.setY(50);
 		bgColor();
 		c.penDown();
 		c.setSpeed(10);
-		//c.setPenWidth(7);
+		changeSpeed();
 		chooseColor();
 		shapeChoice();
 		runAgain();
 		c.hide();
 	}
-	static void drawSquare() {
-		for (int i = 0; i <+ 4; i++) {
-			c.turn(90);
-			c.move(50);
+	static void setLine() {
+		String d = JOptionPane.showInputDialog(null, "Would you like to change the robot's line width from the default?/n(Yes or No)");
+		if(d.equals("Yes") || d.equals("yes") || d.equals("Y") || d.equals("y")) {
+			String a = JOptionPane.showInputDialog(null, "What width do you want the robot to draw with?/n(1-10)/n(1 recomended)");
+			int b = Integer.parseInt(a);
+			if(b>=0 && b<=10) {
+				c.setPenWidth(b);
+			}else {
+				JOptionPane.showMessageDialog(null, "Number input not between 1-10./nSetting to 1.");
+				c.setPenWidth(1);
+			}
+		}else {
+			c.setPenWidth(1);
+		}
+	}
+	static void changeSpeed() {
+		String d = JOptionPane.showInputDialog(null, "Would you like to change the robot's speed from the default?\n(Yes or No");
+		if(d.equals("Yes") || d.equals("yes") || d.equals("Y") || d.equals("y")) {
+			String a = JOptionPane.showInputDialog(null, "What speed do you want the robot to draw?/n(1-100)/n(10 recomended");
+			int b = Integer.parseInt(a);
+			if(b>=0 && b<=100) {
+				c.setSpeed(b);
+			}else {
+				JOptionPane.showMessageDialog(null, "Number input not between 1-100.\nSetting to 100.");
+				c.setSpeed(100);
+			}
+		}else {
+			JOptionPane.showMessageDialog(null, "Setting to 100.");
+			c.setSpeed(100);
 		}
 	}
 	static void chooseColor() {
@@ -94,48 +121,92 @@ public class ObedientRobot {
 	}
 	static void shapeChoice() {
 		String a = JOptionPane.showInputDialog(null, "What shape do you want the robot to draw?\n(square, Triange, or Star)");
-		if(a.equals("square") || a.equals("Square")) {
+		if(a.equals("square") || a.equals("Square") || a.equals("Q")) {
 			drawSquare();
 		}else if(a.equals("triangle") || a.equals("Triangle") || a.equals("T")) {
 			drawTriangle();
-		}else if(a.equals("star") || a.equals("Star")) {
+		}else if(a.equals("star") || a.equals("Star") || a.equals("S")) {
 			drawStar();
+		}else {
+			drawTriangle();
 		}
 	}
 	static void runAgain() {
-		String a = JOptionPane.showInputDialog(null, "Do you want to draw another shape? Input draw to draw another shape, bg to change background color, ink to change pen color, or exit to exit.");
+		String a = JOptionPane.showInputDialog(null, "Do you want to draw another shape? Input draw to draw another shape, bg to change background color, ink to change pen color,\n line to change width, speed to change speed, or exit to exit.");
 		if(a.equals("bg")) {
 			bgColor();
 			runAgain();
 		}else if(a.equals("draw")) {
-			c.setAngle(90);
-			c.move(75);
-			shapeChoice();
-			runAgain();
+			int b = c.getX();
+			int d = c.getY();
+			if(d>=50 && d<=150 && b>=600){
+				c.setX(50);
+				c.setY(d+50);
+				c.setAngle(90);
+				shapeChoice();
+				runAgain();
+			}else if(d>=200 && b>=800){
+				c.setX(50);
+				c.setY(d+50);
+				c.setAngle(90);
+				shapeChoice();
+				runAgain();
+			}else {
+				c.setAngle(90);
+				c.move(75);
+				shapeChoice();
+				runAgain();
+			}
 		}else if(a.equals("ink")) {
 			chooseColor();
 			runAgain();
 		}else if(a.equals("exit")) {
 			System.exit(1);
+		}else{
+			int b = c.getX();
+			int d = c.getY();
+			if(d>=50 && d<=150 && b>=600){
+				c.setX(50);
+				c.setY(d+50);
+				c.setAngle(90);
+				shapeChoice();
+				runAgain();
+			}else if(d>=200 && b>=800){
+				c.setX(50);
+				c.setY(d+50);
+				c.setAngle(90);
+				shapeChoice();
+				runAgain();
+			}else {
+				c.setAngle(90);
+				c.move(75);
+				shapeChoice();
+				runAgain();
+			}
 		}
 	}
 	static void drawTriangle() {
-		c.setAngle(150+180);
+		c.show();
+		c.setAngle(150+60);
 		c.move(50);
 		c.turn(240);
 		c.move(50);
 		c.turn(240);
 		c.move(50);
 		c.hide();
-		c.turn(240);
-		c.move(50);
-		c.turn(360);
 	}
 	static void drawStar() {
-		c.setAngle(160);
+		c.setAngle(165);
 		for(int i=0; i<5; i++) {
 			c.move(50);
 			c.turn(144);
 		}
 	}
+	static void drawSquare() {
+		for (int i = 0; i <+ 4; i++) {
+			c.turn(90);
+			c.move(50);
+		}
+	}
 }
+
